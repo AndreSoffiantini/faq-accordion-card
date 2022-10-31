2bfbc817d7c6abde9f5fc9495854e492b49d97f9
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-10-31 19:21:43 +0100

    Add click effect on faq section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import arrowDown from "./images/icon-arrow-down.svg";
 import boxDesktop from "./images/illustration-box-desktop.svg";
 
@@ -8,32 +10,61 @@ function App() {
       faq: " How many team members can I invite?",
       answer:
         "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+      clicked: false,
     },
     {
       id: 1,
       faq: " What is the maximum file upload size?",
       answer:
         "No more than 2GB. All files in your account must fit your allotted storage space.",
+      clicked: false,
     },
     {
       id: 2,
       faq: " How do I reset my password?",
       answer:
         "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+      clicked: false,
     },
     {
       id: 3,
       faq: " Can I cancel my subscription?",
       answer:
         "Yes! Send us a message and we’ll process your request no questions asked.",
+      clicked: false,
     },
     {
       id: 4,
       faq: " Do you provide additional support?",
       answer:
         "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+      clicked: false,
     },
   ];
+
+  //const faqClicked = true;
+
+  const [faqClicked, setFaqClicked] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const clickHandler = (props) => {
+    const newFaqClicked = faqClicked.map((item, index) => {
+      if (index === props) {
+        item = true;
+      } else {
+        item = false;
+      }
+
+      return item;
+    });
+
+    setFaqClicked(newFaqClicked);
+  };
 
   return (
     <div className="App">
@@ -44,10 +75,128 @@ function App() {
         <div className="col-2">
           <h1>FAQ</h1>
           <div className="faq-section">
-            <div className="faq-element">
-              <span className="faq-text">{faqData[0].faq}</span>
-              <div>
-                <img src={arrowDown} alt="arrow-down" />
+            <div
+              className="faq-element"
+              onClick={() => clickHandler(faqData[0].id)}
+            >
+              <div className="faq-row">
+                <span className={faqClicked[0] ? "text-bold" : null}>
+                  {faqData[0].faq}
+                </span>
+                <div>
+                  <img
+                    src={arrowDown}
+                    className={faqClicked[0] ? "rotate-arrow" : null}
+                    alt="arrow"
+                  />
+                </div>
+              </div>
+              <div
+                className={
+                  "answer-text " + (faqClicked[0] ? "display-block" : null)
+                }
+              >
+                {faqData[0].answer}
+              </div>
+            </div>
+
+            <div
+              className="faq-element"
+              onClick={() => clickHandler(faqData[1].id)}
+            >
+              <div className="faq-row">
+                <span className={faqClicked[1] ? "text-bold" : null}>
+                  {faqData[1].faq}
+                </span>
+                <div>
+                  <img
+                    src={arrowDown}
+                    className={faqClicked[1] ? "rotate-arrow" : null}
+                    alt="arrow"
+                  />
+                </div>
+              </div>
+              <div
+                className={
+                  "answer-text " + (faqClicked[1] ? "display-block" : null)
+                }
+              >
+                {faqData[1].answer}
+              </div>
+            </div>
+
+            <div
+              className="faq-element"
+              onClick={() => clickHandler(faqData[2].id)}
+            >
+              <div className="faq-row">
+                <span className={faqClicked[2] ? "text-bold" : null}>
+                  {faqData[2].faq}
+                </span>
+                <div>
+                  <img
+                    src={arrowDown}
+                    className={faqClicked[2] ? "rotate-arrow" : null}
+                    alt="arrow"
+                  />
+                </div>
+              </div>
+              <div
+                className={
+                  "answer-text " + (faqClicked[2] ? "display-block" : null)
+                }
+              >
+                {faqData[2].answer}
+              </div>
+            </div>
+
+            <div
+              className="faq-element"
+              onClick={() => clickHandler(faqData[3].id)}
+            >
+              <div className="faq-row">
+                <span className={faqClicked[3] ? "text-bold" : null}>
+                  {faqData[3].faq}
+                </span>
+                <div>
+                  <img
+                    src={arrowDown}
+                    className={faqClicked[3] ? "rotate-arrow" : null}
+                    alt="arrow"
+                  />
+                </div>
+              </div>
+              <div
+                className={
+                  "answer-text " + (faqClicked[3] ? "display-block" : null)
+                }
+              >
+                {faqData[3].answer}
+              </div>
+            </div>
+
+            <div
+              className="faq-element"
+              onClick={() => clickHandler(faqData[4].id)}
+            >
+              <div className="faq-row">
+                <span className={faqClicked[4] ? "text-bold" : null}>
+                  {faqData[4].faq}
+                </span>
+                <div>
+                  <img
+                    src={arrowDown}
+                    className={faqClicked[4] ? "rotate-arrow" : null}
+                    alt="arrow"
+                  />
+                </div>
+              </div>
+              <div
+                className={
+                  "answer-text " + (faqClicked[4] ? "display-block" : null)
+                }
+              >
+                {faqData[4].answer}
               </div>
             </div>
           </div>
